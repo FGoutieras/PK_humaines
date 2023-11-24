6a82245e440cbad1ea1029165c46cae51ebf7060
GOUTIERAS FLORIAN <florian.goutieras@etu.umontpellier.fr>
2023-11-24 13:01:24 +0100

    premier commentaire

--- script_PK.py
+++ script_PK.py
@@ -2,12 +2,12 @@ import csv
 import os
 
 cmd.run('fetch_mmcif.py')
-#cmd.set('fetch_path', cmd.exp_path('./cif'), quiet=0)
+# cmd.set('fetch_path', cmd.exp_path('./cif'), quiet=0)
 
 def import_file(fichier,col):
     liste_struct=[] 
     with open(fichier) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')    
+        csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
             if line_count<2:
@@ -23,6 +23,6 @@ fetch_mmcif('2GU8', assembly=1)
 
 for struct in list[0:2]:
     fetch_mmcif(struct, assembly=1)
-    cmd.align(struct,"2GU8")
+    cmd.align(struct,"2GU8") #aligner la chaine qui correspond à la PKACA humaine, en récupérant le code sur la pdb
 os.chdir("..")    
 cmd.zoom()
